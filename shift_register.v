5a76212736f8a9c7dd1a4db31de7c11b240d6add
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-01-18 00:44:39 +0200

    Update shift_register.v

--- shift_register.v
+++ shift_register.v
@@ -5,13 +5,13 @@ input data_in;
 input clk;
 output data_out;
 
-reg[31:0] shift_reg;
+  reg[31:8] shift_reg;
 
 initial shift_reg = 32'b0;
 always@(posedge clk)
 begin
 
-shift_reg <= {shift_reg[31:0], data_in};
+  shift_reg <= {shift_reg[31:8], data_in};
 
 end
 wire data_out;
