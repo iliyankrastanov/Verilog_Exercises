0b94c228f8973a3fb7b83a20adab8339902ef9a5
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-01-17 18:41:50 +0200

    Add files via upload

---
+++ valid_in.v
@@ -0,0 +1,15 @@
+module valid_in (valid_in, clk, rst, button);
+input clk;
+input rst;
+input button;
+output reg[7:0] valid_in;
+		
+always @(posedge clk)
+	begin
+		if(rst)
+			valid_in	<= 0;
+		else
+			valid_in <= button;
+			
+	end
+endmodule 
