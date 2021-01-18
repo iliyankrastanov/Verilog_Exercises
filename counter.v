dd320e6ed884682717fe335364fd32f17d810aa3
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-01-18 14:24:24 +0200

    Update counter.v

--- counter.v
+++ counter.v
@@ -9,17 +9,20 @@ output reg count;
 valid_in(.clk(clk), .rst(rst),.button(click));
 			
 	always @(posedge clk)
+		begin
 		if(rst)
 				valid_in1 <= 8'b0;
 			else if (load)
 					valid_in1 <= load;
 			else  
 				valid_in1 <= valid_in1 + 8'b1;
-				
+		end
 	always @ valid_in1
+		begin
 			if (valid_in1 == load)
 				count = 1'b1;
 			else
 				count = 1'b0;
+		end
 	
 endmodule
