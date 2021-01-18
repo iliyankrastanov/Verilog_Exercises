89b7b1bf4d1a7c285bc2657156f5735b36098e8d
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-01-18 10:07:02 +0200

    Add files via upload

---
+++ memory_dr.v
@@ -0,0 +1,20 @@
+  
+module memory_dr(memory_out, data, write, address, clk);
+input clk;
+input reg [31:0]data;
+input write;
+input address;
+output reg [31:0] memory_out;
+
+						
+	reg [31:0] memory [63:0];
+	
+	always @(posedge clk) 
+	begin
+		if(write) begin
+			memory[address] <= data;
+		end
+			memory_out <=memory[address];
+	end
+	
+endmodule
