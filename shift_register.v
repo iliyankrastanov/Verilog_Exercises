54e3b0feb2c6c6d4da10fd410f31c3ac810aa038
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-01-18 15:00:58 +0200

    Update shift_register.v

--- shift_register.v
+++ shift_register.v
@@ -9,7 +9,7 @@ module shift_register(data_out, data_in, valid_in, clk);
   reg[7:1] shift_reg;
 
 initial shift_reg = 8'b0;
-  always@(posedge clk & valid_in)
+  always@(valid_in)
 begin
 
   shift_reg <= {4{shift_reg[7:1], data_in}};
