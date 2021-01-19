0b672eff3ef1a36ac0d9d571dfc621c54279d776
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-01-19 13:12:50 +0200

    Update memory_driver.v

--- memory_driver.v
+++ memory_driver.v
@@ -15,16 +15,16 @@ always @(posedge clk)
 				current_state <= #1 state_1;
 			else
 				current_state <= #1 next_state;
-
+		end
 	always @(current_state or data_in or write or addr)
 			begin
 			next_state = current_state;
 				case (current_state)
 					state_1:
 						if (data_in == 1)
-							next_state <= state_2;
+							next_state = state_2;
 						else
-							next_state <= state_1;
+							next_state = state_1;
 					
 					state_2:
 						if (data_in == 1 && data_in > `memory_dr)
@@ -32,13 +32,13 @@ always @(posedge clk)
 								write = write +1;
 								memory[addr] = data_in; 
 								addr = addr + 1;
-								next_state <= state_3;
+								next_state = state_3;
 							end
 						else if (data_in == 1 && data_in < `memory_dr) 
 							begin
 								write = write - 1;
 								memory_full = memory[addr];
-								next_state <= state_1;
+								next_state = state_1;
 							end
 							
 					state_3:
@@ -46,11 +46,11 @@ always @(posedge clk)
 							begin
 								write = 0;
 								addr = 0;
-								next_state <= state_1;
+								next_state = state_1;
 							end
-						else next_state <= state_3;
+						else next_state = state_3;
 						
-						default : next_state <= state_1;
+						default : next_state = state_1;
 					endcase
 				end
 				
