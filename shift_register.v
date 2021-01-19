121f1573238def31941cfda9e2830f9b68855107
iliyankrastanov <77394044+iliyankrastanov@users.noreply.github.com>
2021-01-19 09:34:04 +0200

    Update shift_register.v

--- shift_register.v
+++ shift_register.v
@@ -1,10 +1,11 @@
 
-module shift_register(data_out, data_in, valid_in, clk);
+module shift_register(data_out,valid_fifo data_in, valid_in, clk);
 
   input clk;
   input [7:0] data_in;
   input [7:0] valid_in;
   output [31:0]data_out;
+  output valid_fifo;
 
   reg[31:0] shift_reg;
 
@@ -16,7 +17,11 @@ begin
 
 end
 wire data_out;
-  assign data_out = shift_reg;
+assign data_out = shift_reg;
 
-
+  always @(posedge clk or valid_in)
+    begin
+				shift_reg <= shift_reg + 1;
+        end
+  assign valid_fifo = {4{shift_reg}};
 endmodule
